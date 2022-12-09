213f4084468d47c1b23fd5e2faceeea8f1a4b488
Hemlata <hemalata.ahire@nium.com>
2022-12-09 18:30:48 +0530

    Review chnages

--- src/main/java/com/parcel/parcelcosting/enums/MessageCode.java
+++ src/main/java/com/parcel/parcelcosting/enums/MessageCode.java
@@ -1,17 +1,13 @@
 package com.parcel.parcelcosting.enums;
 
 public class MessageCode {
-    public static String VOUCHER_EXPIRED = "Voucher is expired!!";
-    public static String INVALID_VOUCHER_CODE = "Voucher code is not valid!!";
     public static final String INVALID_WEIGHT = "Weight must be greater that 0";
     public static final String INVALID_WEIGHT_NULL = "Weight cannot be null";
     public static final String WEIGHT_LIMIT_EXCEEDED = "Weigth is greater that 50kg";
     public static final String INVALID_HEIGHT = "height must be greater that 0";
     public static final String INVALID_HEIGHT_NULL = "height cannot be null";
-
     public static final String INVALID_WIDTH = "Width must be greater that 0";
     public static final String INVALID_WIDTH_NULL = "Width cannot be null";
-
     public static final String INVALID_LENGTH = "length must be greater that 0";
     public static final String INVALID_LENGTH_NULL = "length cannot be null";
 }
